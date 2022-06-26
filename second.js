b0bde179941b41eb9571e2607180ec9549e2f032
dhanvi2001 <dhanvitrivedi28@gmail.com>
2022-06-26 19:20:44 +0530

    adding second.js

---
+++ second.js
@@ -0,0 +1,16 @@
+var num = prompt("Enter the number");
+var myArr = num.split("").map((num) => {
+    return Number(num)
+})
+
+console.log(myArr)
+let sum = 0;
+
+function add() {
+
+    for (i = 0; i < myArr.length; i++) {
+        sum = sum + myArr[i];
+    }
+    console.log(sum);
+}
+add()
