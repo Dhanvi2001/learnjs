64332465e6a1b2c4104ff167ee94093d0cb2f801
dhanvi2001 <dhanvitrivedi28@gmail.com>
2022-06-27 13:23:21 +0530

    third.js updated

--- third.js
+++ third.js
@@ -1,7 +1,22 @@
-const number = Array(prompt("enter number to be added"));
+var num = prompt("Enter the number");
+var myArr = num.split(" ").map((num) => {
+    return Number(num)
+})
+console.log(myArr)
+let sum = 0;
+let avg;
 
-function max() {
+function average() {
 
+    for (i = 0; i < myArr.length; i++) {
+        sum = sum + myArr[i];
+    }
+    avg = sum / 2
+    console.log(avg);
 }
+var max = Math.max(...myArr);
+var min = Math.min(...myArr);
 
-function min() {
+console.log(max);
+console.log(min);
+average()
