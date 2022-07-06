1a766aae85f5053ae6d3f4d6e4614c51e151e9a5
dhanvi2001 <dhanvitrivedi28@gmail.com>
2022-07-06 12:53:18 +0530

    1.js

--- express/1.js
+++ express/1.js
@@ -2,13 +2,18 @@ var express = require('express');
 var os = require('os');
 
 var app = express();
-
+app.set('view engine', 'ejs');
 app.get('/home', function(req, res) {
     res.sendFile(os.homedir + '/learnjs/home.html');
 });
 
 app.get('/contact', function(req, res) {
     res.sendFile(os.homedir + '/learnjs/contact.html');
+});
+
+app.get('/profile/:name', function(req, res) {
+    var data = { age: 15, job: 'baker', hobbies: ['playing', 'cooking', 'swimming'] };
+    res.render(os.homedir + '/learnjs/view/profile.ejs', { person: req.params.name, data: data });
 });
 
 app.listen(3000);
