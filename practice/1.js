ede735abc8bb9c265a164ef016d2f6c031bc1d23
dhanvi2001 <dhanvitrivedi28@gmail.com>
2022-07-11 16:23:31 +0530

    1.js

--- practice/1.js
+++ practice/1.js
@@ -4,16 +4,16 @@ var os = require('os');
 var app = express();
 app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
-    res.render(os.homedir + '/learnjs/view/partials/index.ejs');
+    res.render(os.homedir + '/learnjs/practice/view/index.ejs');
 });
 
 app.get('/contact', function(req, res) {
-    res.render(os.homedir + '/learnjs/view/partials/contact.ejs');
+    res.render(os.homedir + '/learnjs/practice/view/contact.ejs');
 });
 
 app.get('/profile/:name', function(req, res) {
     var data = { age: 15, job: 'baker', hobbies: ['playing', 'cooking', 'swimming'] };
-    res.render(os.homedir + '/learnjs/view/profile.ejs', { person: req.params.name, data: data });
+    res.render(os.homedir + '/learnjs/practice/view/profile.ejs', { person: req.params.name, data: data });
 });
 
 app.listen(3000);
