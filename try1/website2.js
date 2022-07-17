8042ac74c3ccab960d774abb762eac9798e31b49
dhanvi2001 <dhanvitrivedi28@gmail.com>
2022-07-17 17:34:04 +0530

    website2.js

--- try1/website2.js
+++ try1/website2.js
@@ -1,10 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var path = require('path');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 var staticPath = path.join(__dirname, '../public');
 var home = path.join(__dirname, '../public/ejs/home.ejs');
 var contact = path.join(__dirname, '../public/ejs/contact.ejs');
 var profile = path.join(__dirname, '../public/ejs/profile.ejs');
+var contact_sucess = path.join(__dirname, '../public/ejs/contact_sucess.ejs');
 
 var app = express();
 app.use(express.static(staticPath));
@@ -13,8 +17,13 @@ app.get('/', function(req, res) {
     res.render(home);
 });
 
-app.get('/contact/', function(req, res) {
-    res.render(contact);
+app.get('/contact', function(req, res) {
+    res.render(contact, { qs: req.query });
+});
+
+app.post('/contact', urlencodedParser, function(req, res) {
+    console.log(req.body);
+    res.render(contact_sucess, { data: req.body });
 });
 
 app.get('/profile/:name', function(req, res) {
@@ -22,4 +31,4 @@ app.get('/profile/:name', function(req, res) {
     res.render(profile, { person: req.params.name, data: data });
 });
 
-app.listen(3000);
+app.listen(3010);
